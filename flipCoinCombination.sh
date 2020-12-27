83a415d9ac1b7f529252081b6e1388bc045cf908
joel3216 <joeljaxon3216@gmail.com>
2020-12-27 17:52:59 +0530

    REFACTOR. added code to store doublet combinations and calculate the percentages of each combination, then store it in a dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -x
+#! /bin/bash
 
 # flip coin combination assignment
 heads=1
@@ -9,14 +9,24 @@ declare -A singletPercent
 
 for ((i=0;i< $limit;i++))
 do
-	flipCoin=$((RANDOM%2))
-	if [[ $flipCoin -eq $heads ]]
+	flipCoin1=$((RANDOM%2))
+	flipCoin2=$((RANDOM%2))
+	if [[ $flipCoin1 -eq $heads ]]
 	then
-		singlets[$i]="H"
+		flipCoin1="H"
 	else
-		singlets[$i]="T"
-
+		flipCoin1="T"
 	fi
+
+	if [[ $flipCoin2 -eq $heads ]]
+	then
+		flipCoin2="H"
+	else
+		flipCoin2="T"
+	fi
+
+	singlets[$i]=$flipCoin1$flipCoin2
+
 done
 
 for combination in ${singlets[@]}
@@ -28,4 +38,5 @@ echo ${singlets[@]}
 for combination in ${!singletPercent[@]}
 do
 	singletPercent[$combination]=`awk 'BEGIN {print ('${singletPercent[$combination]}'/'$limit'*100)}'`
+	echo $combination" "${singletPercent[$combination]}"%"
 done
