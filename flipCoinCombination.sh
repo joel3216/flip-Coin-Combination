e26199b4341ba6308cb2c746940691992e2d976d
joel3216 <joeljaxon3216@gmail.com>
2020-12-27 17:37:47 +0530

    ADD. added code to calculate the percentage of singlets and store it in a dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,13 +1,31 @@
-#! /bin/bash
+#! /bin/bash -x
 
 # flip coin combination assignment
 heads=1
 tails=0
-flipCoin=$((RANDOM%2))
+limit=10
+declare -A singlets
+declare -A singletPercent
 
-if [[ $flipCoin -eq $heads ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+for ((i=0;i< $limit;i++))
+do
+	flipCoin=$((RANDOM%2))
+	if [[ $flipCoin -eq $heads ]]
+	then
+		singlets[$i]="H"
+	else
+		singlets[$i]="T"
+
+	fi
+done
+
+for combination in ${singlets[@]}
+do
+	singletPercent[$combination]=$((${singletPercent[$combination]}+1))
+done
+
+echo ${singlets[@]}
+for combination in ${!singletPercent[@]}
+do
+	singletPercent[$combination]=`awk 'BEGIN {print ('${singletPercent[$combination]}'/'$limit'*100)}'`
+done
